a7339bc0c8d55256219391b018e6e1eb01e8691a
duytngo <duy.ngo94@gmail.com>
2016-04-02 13:00:09 -0700

    Changed index.php back

--- index.php
+++ index.php
@@ -2,17 +2,7 @@
 <html>
 
 	<head>
-		<style>
-			.signup{
-				position: absolute;
-				right: 0px;
-				width: 100px
-			}
-			.signin{
-				position: absolute;
-				right: 0px;
-			}
-		</style>
+
 	</head>
 
 	<body>
@@ -21,12 +11,7 @@
 	header("Location: src/models/index.html"); 
 
 	?>
-		<h1 align="center">Image Rating</h1>  
-		<a href="./signup.php" class="signup">Sign up</a>
-		<a href="./signin.php" class="signin">Sign in</a>
-		
-		<h2>Recent</h2>
-		<h2>Popularity</h2>
+	
 	</body>
 
 </html>
