6239ccf769aea4dd29b7972b4a6f02396e2bb718
Stephen <sing.stephen95@gmail.com>
2016-04-03 21:23:26 -0700

    Added Post table

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -13,8 +13,22 @@
 		u_pwd VARCHAR(50) NOT NULL,
 		PRIMARY KEY (u_id)
 	)";
+
+	$createPostTable = "CREATE TABLE POST(
+		u_id INT NOT NULL,
+		f_name VARCHAR(50) NOT NULL,
+		l_name VARCHAR(50) NOT NULL,
+		date_submitted DATE NOT NULL,
+		caption TEXT NOT NULL,
+		rating INT NOT NULL,
+		PRIMARY KEY (u_id)
+	)";
+
+	
 	
 	mysqli_query($createUserTable);
+	mysqli_query($createPostTable);
+
  
  
 	
