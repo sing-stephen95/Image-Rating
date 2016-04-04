15040b8bb944527e7cfc2ab42b0fb5fc7d5414f0
duytngo <duy.ngo94@gmail.com>
2016-04-04 10:49:28 -0700

    Updated constants to Config.php

--- src/configs/Config.php
+++ src/configs/Config.php
@@ -1,5 +1,8 @@
 <?php
-
+	$server = ini_get("mysql.default_host");
+	$user = ini_get("mysql.default_user");
+	$pwd =  ini_get("mysql.default_password");
+	$db =  "HW3";
 	/*  Contstants for things like database user, password, host, port, etc. */
 	
 
