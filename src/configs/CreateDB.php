b5a266317f0edd73bc2366f4349546f3c5bc8a51
Duy Ngo <duy.ngo94@gmail.com>
2016-04-03 20:48:36 -0700

    Updated CreateDB.php

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -1,6 +1,23 @@
 <? php
  
+	require_once('./Config.php');
+	require_once('../mySqlConnection.php');
+	
+	$conn = mysqli_connect($server, $user, $pwd, $db);
+
+	$createUserTable = "CREATE TABLE USER(
+		u_id INT NOT NULL,
+		f_name VARCHAR(50) NOT NULL,
+		l_name VARCHAR(50) NOT NULL,
+		email VARCHAR(255) NOT NULL,
+		u_pwd VARCHAR(50) NOT NULL,
+		PRIMARY KEY (u_id)
+	)";
+	
+	mysqli_query($createUserTable);
  
+ 
+	
  /*can be run from the command-line to make a good initial database.*/
  
  
