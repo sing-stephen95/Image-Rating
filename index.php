b157c830ce43e1c9e9bca0ea7b633e671c040690
Duy Ngo <duy.ngo94@gmail.com>
2016-04-10 01:04:51 -0700

    Revert "Initial entry point (MVC)"
    This reverts commit d6bfab445115f4602c4497280962def652afbb12.

--- index.php
+++ index.php
@@ -1,23 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-namespace Image_Rating;
+	<head>
 
-require_once "src/controllers/MainPageController.php";
+	</head>
 
-define("NS_BASE", "Image_Rating\\");
-define(NS_BASE . "NS_CONTROLLERS", "Image_Rating\\controllers\\");
-define(NS_BASE . "NS_VIEWS", "Image_Rating\\views\\");
+	<body>
+	<?php
 
-$allowed_controllers = ["signup", "signin", "upload", "main"];
+	header("Location: src/models/index.html"); 
 
-if(!empty($_REQUEST['c']) && in_array($_REQUEST['c'], $allowed_controllers)){
-	$controller_name = NS_CONTROLLERS . ucfirst($_REQUEST['c'])."Controller";
-}	else{
-	$controller_name = NS_CONTROLLERS . "MainPageController";
-}
+	?>
 	
-$controller = new $controller_name();
+	</body>
 
-$controller->processRequest();
-
-
+</html>
--- src/views/View.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Image_Rating\views;
-
-
-abstract class View
-{
-    public abstract function render($data);
-}
