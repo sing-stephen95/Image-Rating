38f85b241e4d5f0f5e434420c6de8b255580daab
Duy Ngo <duy.ngo94@gmail.com>
2016-04-10 01:05:24 -0700

    Controller update

---
+++ src/controllers/Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Image_Rating\controllers;
+
+use Image_Rating as B;
+
+abstract class Controller{
+	public function view($name){
+		static $loaded_views = [];
+		if(!empty($loaded_views[$name])){
+			return $loaded_views[$name];
+		}
+		
+		$class_name = ucfirst($name) . "View";
+		$full_name = B\NS_VIEWS. $class_name;
+		$view_folder = __DIR__."/../views/";
+		$path_name = $view_folder. $class_name . ".php";
+		if(file_exists($path_name)){
+		require_once $path_name;
+		$loaded_views[$name] = new $full_name();
+		return $loaded_views[$name];
+		}
+		
+		return false;
+	}
+}
