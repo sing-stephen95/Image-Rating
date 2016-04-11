d7e49a380da82d7b627668bd69743469f5488804
Duy Ngo <duy.ngo94@gmail.com>
2016-04-10 20:33:27 -0700

    database model

---
+++ src/models/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Image_Rating\models;
+
+require_once "./src/configs/Config.php";
+
+class Database{
+	public function connect(){
+		$conn = mysqli_connect($server, $user, $pwd, $db);
+		return $conn;
+	}
+}
+
+?>
