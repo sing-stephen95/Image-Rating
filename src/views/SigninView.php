37fe8682b626933294d4767ec0c8e193d71861f8
Duy Ngo <duy.ngo94@gmail.com>
2016-04-10 22:04:32 -0700

    Inc signinview

--- src/views/SigninView.php
+++ src/views/SigninView.php
@@ -2,7 +2,12 @@
 
 namespace Image_Rating\views;
 
+session_start();
+
+use Image_Rating as B;
+
 require_once "View.php";
+//require_once "../../models/Database.php";
 
 class SigninView extends View{
 
@@ -17,7 +22,7 @@ class SigninView extends View{
 
 			<h1 align="center">Sign In</h1>
 
-			<form id="signin" action="validate.php" method="post" align="center">
+			<form id="signin" action="./index?c=signin" method="post" align="center">
 				<fieldset>
 				
 					<input type="hidden" name="submitted" id="submitted" value="1"/>
@@ -35,6 +40,32 @@ class SigninView extends View{
 			</body>
 		</html>
 		<?php
+		if(isset($_POST['Submit'])){
+			$user_name = $_POST['username'];
+			$pass = $_POST['password'];
+			
+			$check_log="SELECT * FROM USERS WHERE u_name='$user_name' AND u_pwd='$pass'";
+			
+			$nm_name = B\NS_CONTROLLERS."Database";
+			$db = new $nm_name();
+			$conn = $db->connect();
+			$check = mqsqli_query($conn, $check_log);
+			
+			if(mysqli_num_rows($run))  
+			{  
+			echo "<script>window.open('welcome.php','_self')</script>";  
+	  
+			$_SESSION['email']=$user_email;//here session is used and value of $user_email store in $_SESSION.  
+  
+			}  
+			else{
+				echo "<script>alert('Email or password is incorrect!')</script>";
+			}
+		}
+		else{
+			echo "fail";
+		}
 	}
+	
 
 }
